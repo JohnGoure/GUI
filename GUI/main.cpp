e8bc1447918e1943974949a4ff43bc50f948adbf
johngoure <johnsgithub@gmail.com>
2014-09-04 16:32:33 -0500

    GUI Upload
    Uploading GUI

--- GUI/main.cpp
+++ GUI/main.cpp
@@ -6,13 +6,59 @@
 //  Copyright (c) 2014 Framed Games. All rights reserved.
 //
 
+
 #include <iostream>
+#include <OpenAL/al.h>
+#include <OpenAL/alc.h>
+#include <OpenGL/gl.h>
+#include <GLUT/GLUT.h>
+#include <glfw3.h>
 
-int main(int argc, const char * argv[])
+using namespace std;
+
+const int WINDOW_WIDTH = 1200;
+const int WINDOW_HEIGHT = 720;
+
+const char* WINDOW_TITLE = "OOP Assignment 2 Window";
+
+void display ();
+
+
+int main(int argc, char * argv[])
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    glutInit(&argc, argv);
+    glutInitWindowPosition(0, 0);
+    glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+    
+    glutCreateWindow(WINDOW_TITLE);
+    
+    glutDisplayFunc(display);
+    
+    glutMainLoop();
+    
+    
+    
     return 0;
 }
 
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+    
+    glBegin(GL_TRIANGLES);
+    
+    glColor3f(1.0, 0.0, 0.0);
+    glVertex2f(-1, -1);
+    glColor3f(0.0, 1.0, 0.0);
+    glVertex2f(0.0, 1);
+    glColor3f(0.0, 0.0, 1.0);
+    glVertex2f(1, -1);
+    
+    glEnd();
+    
+    glutSwapBuffers();
+    
+}

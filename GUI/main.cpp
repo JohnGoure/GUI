ff9d9177371eb6be1bcbb5c75f3b9253d0eb8478
johngoure <johnsgithub@gmail.com>
2014-09-04 18:45:03 -0500

    added comments

--- GUI/main.cpp
+++ GUI/main.cpp
@@ -25,16 +25,25 @@ void display ();
 
 int main(int argc, char * argv[])
 {
-    
+    // Initialize GLUT
     glutInit(&argc, argv);
+    
+    // Position where you want the window to be on the screen
     glutInitWindowPosition(0, 0);
+    
+    // Set the window width and height
     glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    
+    // Set the windows display color and refresh rate
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
     
+    // Set a title for the window
     glutCreateWindow(WINDOW_TITLE);
     
+    // Call a function to display something inside the window
     glutDisplayFunc(display);
     
+    // Run a continuous loop that displays the window
     glutMainLoop();
     
     
@@ -44,11 +53,16 @@ int main(int argc, char * argv[])
 
 
 void display() {
+    // set the refresh rate of the object being displayed inside the window
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    // I haven't figured this part out yet
     glLoadIdentity();
     
+    // Draws a triangle in the window
     glBegin(GL_TRIANGLES);
     
+    // Sets the Triangles Color
     glColor3f(1.0, 0.0, 0.0);
     glVertex2f(-1, -1);
     glColor3f(0.0, 1.0, 0.0);
@@ -56,8 +70,10 @@ void display() {
     glColor3f(0.0, 0.0, 1.0);
     glVertex2f(1, -1);
     
+    // I don't know
     glEnd();
     
+    // I don't know
     glutSwapBuffers();
     
 }

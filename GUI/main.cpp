ec48c5c71b88c9afc2a828f78ce415dfc87468d4
johngoure <johnsgithub@gmail.com>
2014-09-04 16:24:26 -0500

    Initial Commit

---
+++ GUI/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  GUI
+//
+//  Created by John Goure on 9/4/14.
+//  Copyright (c) 2014 Framed Games. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
